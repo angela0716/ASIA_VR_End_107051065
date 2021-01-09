b6c6b1a3f3f75654a3dc12cf0b2d5abb59ab5476
angela0716 <72055214+angela0716@users.noreply.github.com>
2021-01-10 00:14:52 +0800

    老師救救救我

--- VR_End_107051065/Assets/Scripts/ScoreManager.cs
+++ VR_End_107051065/Assets/Scripts/ScoreManager.cs
@@ -7,6 +7,14 @@ public class ScoreManager : MonoBehaviour
     public Text textScore;
     [Header("分數")]
     public int score;
+    [Header("擊球音效")]
+    public AudioClip soundIn;
+    private AudioSource aud;
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
        if(other.tag =="棒球")
@@ -18,5 +26,6 @@ public class ScoreManager : MonoBehaviour
     {
         score += 1;
         textScore.text = "得分:" + score;
+        aud.PlayOneShot(soundIn, 1.5f);
     }
 }
