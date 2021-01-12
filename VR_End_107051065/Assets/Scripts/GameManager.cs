2591613f9095b2937ff1f9896dc8213c377dc710
angela0716 <72055214+angela0716@users.noreply.github.com>
2021-01-12 15:21:45 +0800

    期末

---
+++ VR_End_107051065/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// 重新
+    /// </summary>
+    public void RestarGame()
+    {
+        SceneManager.LoadScene("九宮格");
+    }
+    /// <summary>
+    /// 離開
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- VR_End_107051065/Assets/Scripts/ScoreManager.cs
+++ VR_End_107051065/Assets/Scripts/ScoreManager.cs
@@ -6,7 +6,13 @@ public class ScoreManager : MonoBehaviour
     [Header("分數介面")]
     public Text textScore;
     [Header("分數")]
-    public int score;
+    public static int score;
+    [Header("分數")]
+    public int score1;
+    [Header("分數2")]
+    public int score2;
+    [Header("分數3")]
+    public int score3;
     [Header("擊球音效")]
     public AudioClip soundIn;
     private AudioSource aud;
@@ -19,13 +25,29 @@ public class ScoreManager : MonoBehaviour
     {
        if(other.tag =="棒球")
         {
+            transform.GetChild(0).GetComponent<Collider>().enabled = false;
             AddScore();
+            AddScore2();  
+            AddScore3();
         }
     }
     private void AddScore()
     {
-        score += 1;
+        score += score1;
         textScore.text = "得分:" + score;
-        aud.PlayOneShot(soundIn, 1.5f);
+        aud.PlayOneShot(soundIn, Random.Range(0.1f,1f));
     }
+    private void AddScore2()
+    {
+        score += score2;
+        textScore.text = "得分:" + score;
+     
+    } 
+    private void AddScore3()
+    {
+        score += score3;
+        textScore.text = "得分:" + score;
+     
+    }
+
 }
